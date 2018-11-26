62a4f9cf62c8aa749dff6b3de582cd5687bd86b1
Igor Souto <igor_souto@outlook.com>
2018-11-26 18:00:46 -0200

    challenge-3 initialized

---
+++ exercise-3/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#exercise',
+    data: {
+        value: 0
+    },
+    computed: {
+        result: function(){
+          return this.value === 37 ?  'Done' : 'Not there yet';
+      }
+    }
+});
