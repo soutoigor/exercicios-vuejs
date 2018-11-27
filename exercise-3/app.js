65c8eee7f49edc29acaa25872dda431528b71a9e
soutoigor <igor_souto@outlook.com>
2018-11-26 22:41:22 -0200

    exercicio-3 concluído

--- exercise-3/app.js
+++ exercise-3/app.js
@@ -1,11 +1,20 @@
 new Vue({
     el: '#exercise',
     data: {
-        value: 0
+        value: 0,
+        step: 0
     },
     computed: {
         result: function(){
           return this.value === 37 ?  'Done' : 'Not there yet';
+      }
+    },
+    watch: {
+    	result: function(){
+     	var vm = this;
+      	setTimeout(function(){
+        	vm.value = 0;
+        }, +vm.step)
       }
     }
 });
