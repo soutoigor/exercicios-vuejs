fe71c7fe30b4215e661454282a70ed1d37486d04
soutoigor <igor_souto@outlook.com>
2018-11-27 00:40:03 -0200

    exercicio3 - concluído

--- exercise-3/app.js
+++ exercise-3/app.js
@@ -1,20 +1,47 @@
 new Vue({
     el: '#exercise',
     data: {
-        value: 0,
-        step: 0
+          attachColor: false,
+      hasClass: 'font-size: 20px',
+      color: '',
+      insertClass: '',
+      isTrue: '',
+      insertStyle: '',
+      progress: 0
+    },
+    methods: {
+      startEffect: function() {
+          var vm = this;
+        setInterval(function(){
+      console.log(vm.attachColor)
+          vm.attachColor = !vm.attachColor;
+      },1500);
+          
+      },
+      startProgress: function(){
+          var vm = this;
+        vm.progress = 0;
+        var progressBarInterval = setInterval(function(){
+            vm.progress++;
+          if (vm.progress === 100)
+              clearInterval(progressBarInterval);
+        }, 75);
+      }
     },
     computed: {
-        result: function(){
-          return this.value === 37 ?  'Done' : 'Not there yet';
-      }
-    },
-    watch: {
-    	result: function(){
-     	var vm = this;
-      	setTimeout(function(){
-        	vm.value = 0;
-        }, +vm.step)
+        effectColors: function(){
+          return {
+            shrink: this.attachColor,
+          highlight: !this.attachColor
+        }
+      },
+      isTrueClass: function(){
+          return this.isTrue === 'true' ? true :
+              false;
+      },
+      progressStyle: function(){
+      return 'width: '+ this.progress+'%; background-color: purple; height: 100%'
       }
     }
-});
+  });
+  
