265ecd31221b9ce1e5d6b2f04bc2dc34a9b873ae
soutoigor <igor_souto@outlook.com>
2018-12-13 00:19:33 -0200

    update exercises. component module

---
+++ components-exercise/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <server-status-header></server-status-header>
+        <hr>
+        <div class="row">
+        <servers-content></servers-content>
+        <server-details></server-details>
+        </div>
+        <hr>
+        <server-status-footer></server-status-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './components/shared/Header.vue';
+    import Servers from './components/servers/ServerStatus.vue';
+    import ServerDetails from './components/servers/ServerDetails.vue';
+    import Footer from './components/shared/Footer.vue';
+
+    export default{
+        components: {
+            'server-status-header': Header,
+            'servers-content': Servers,
+            'server-details': ServerDetails,
+            'server-status-footer': Footer
+        }
+    }
+</script>
+
+<style>
+
+</style>
