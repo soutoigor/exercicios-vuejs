9dc56c187b754d311a206e420c48e621620a4fc7
soutoigor <igor_souto@outlook.com>
2018-12-20 13:24:07 -0200

    components data - Exercise

---
+++ exercise-6/src/components/Server/ServerDetails.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <h2>Server Details</h2>
+        <p><strong>Server selected:</strong> {{server.id}}, <strong>Status: </strong>{{server.status}}</p>
+        <button @click="updateServer">Update Server</button>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: ['Servers'],
+        methods: {
+            updateServer(){
+                this.status = 'Normal';
+                this.$emit('serverNormal', this.status);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ exercise-6/src/components/Server/Servers.vue
@@ -0,0 +1,24 @@
+<template>
+   <ServerList></ServerList>
+</template>
+
+<script>
+import ServerList from 'ServerList.vue';
+    export default {
+        data(){
+            return {
+            servers: [
+                {id: 1, status: 'Normal'},
+                {id: 2, status: 'Critical'},
+                {id: 3, status: 'Slow'},
+                {id: 4, status: 'Unknown'}
+            ]
+            };
+        }
+            
+    }
+</script>
+
+<style>
+
+</style>
