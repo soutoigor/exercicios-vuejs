79602290566fe1e952fbb431013de7963cd14034
Igor Souto <igor_souto@outlook.com>
2018-12-22 01:36:11 -0200

    Add files via upload

---
+++ quote-exercise/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <app-quote>
+                   <h1 slot="title">Title</h1>
+                   <p slot="name">Vai carai</p>
+               </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    export default {
+        components: {
+            appQuote: Quote
+        } 
+    }
+</script>
+
+<style scoped>
+   
+</style>
---
+++ quote-exercise/src/components/Quote.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <slot name="title"></slot>
+        <slot name="name"></slot>
+        <input  type="text">
+        <p>{{ texto }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['quote'],
+
+    data(){
+        return{
+            texto: 'vaicoroi'
+        }
+    }
+}
+</script>
+
+<style scoped>
+* {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;   
+}
+div {
+    border: 1px solid #bbb;
+    margin: 30px auto;
+    padding: 30px;
+    box-shadow: 1px 1px 2px #000;
+    text-align: center;
+    background: -webkit-linear-gradient(left, rgba(139,30,212,1) 0%, rgba(139,30,212,0.86) 44%, rgba(56,86,238,0.76) 74%, rgba(0,123,255,0.76) 94%);
+    }
+h1 {
+    color: #fff;
+    font-size: 3em;
+    font-weight: 500
+}
+p {
+    font-size: 1.6em;
+    color: #fff
+}
+   
+</style>
