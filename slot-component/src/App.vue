cf27b61ca01bdd8fc8ef4dd024f9fa8aea862a21
soutoigor <igor_souto@outlook.com>
2018-12-23 13:27:20 -0200

    Exercicio conlcluido

---
+++ slot-component/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="selectedComponent = 'appBlue',
+                title= 'Im Blue',
+                text = 'Da baa dee da baa da'"
+                 class="btn btn-primary">Load Blue Template</button>
+                <button @click="selectedComponent = 'appGreen',
+                title= 'VAI PARMEEERA',
+                text = '100mundial'"
+                 class="btn btn-success">Load Green Template</button>
+                <button @click="selectedComponent = 'appRed',
+                title= 'MEU CORACAO É VERMELHOO',
+                text = 'vermelho vermelhasso vermelho vermelho'"
+                 class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <component :is="selectedComponent">
+                    <h1>{{title}}</h1>
+                    <p>{{text}}</p>
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        data(){
+            return {
+                    title: 'Im Blue',
+                    text: 'Da baa dee da baa da',
+                    selectedComponent: 'appBlue'
+                                
+            }
+        }
+    }
+</script>
+
+<style>
+    * {
+        color: white;
+    }
+</style>
