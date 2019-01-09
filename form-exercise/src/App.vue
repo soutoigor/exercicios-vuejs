6a4934e660996fe8966d0edd0863e796b64ebd9a
soutoigor <igor_souto@outlook.com>
2019-01-09 15:49:50 -0200

    update

--- form-exercise/src/App.vue
+++ form-exercise/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if="!isSubmitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <!-- Exercise 1 -->
@@ -15,25 +15,43 @@
                     </h1>
                     <hr>
                     <div class="form-group">
+                        <label for="firstName">First Name
+                            <input type="text" name="firstName" id="firstName" class="form-control"
+                            v-model="userData.firstName">
+                        </label>
+                    </div>
+                    <div class="form-group">
+                        <label for="lastName">Last Name
+                            <input type="text" name="lastName" id="lastName" class="form-control"
+                            v-model="userData.lastName">
+                        </label>
+                    </div>
+                    <div class="form-group">
                         <label for="email">Email
-                            <input type="email" name="email" id="email" class="form-control">
+                            <input type="email" name="email" id="email" class="form-control"
+                            v-model="userData.email">
                         </label>
                     </div>
                     <div class="form-group">
                         <label for="password">Password
-                            <input type="password" name="password" id="password" class="form-control">
+                            <input type="password" name="password" id="password" class="form-control"
+                            v-model="userData.password">
                         </label>
                     </div>
                     <div class="form-group">
-                        <label for="yes">Yes
-                            <input type="radio" name="yes" id="yes" class="form-control">
+                        <p><strong>Store in Database?</strong></p>
+                        <label for="yes">
+                            <input type="radio" value="Yes" name="yes" id="yes" class="form-control"
+                            v-model="userData.isStoredInDB">Yes
                         </label>
-                        <label for="no">No
-                            <input type="radio" name="no" id="no" class="form-control">
+                    
+                        <label for="no">
+                            <input type="radio" value="No" name="no" id="no" class="form-control"
+                            v-model="userData.isStoredInDB">No
                         </label>
                     </div>
 
-
+                    <button @click.prevent="isSubmitted = true" class="btn btn-primary">Submit</button>
 
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
@@ -46,17 +64,20 @@
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: <span><app-full-name></app-full-name></span></p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Store in Database?: {{ userData.isStoredInDB }}</p>
+                    </div>
+                    <div class="panel-footer">
+                        <button @click="isSubmitted = false" class="btn btn-danger">< Back</button>
                     </div>
                 </div>
             </div>
@@ -65,7 +86,23 @@
 </template>
 
 <script>
+    import FullName from './FullName.vue';
     export default {
+        data(){
+            return{
+                userData: {
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    password: '',
+                    isStoredInDB: false
+                },
+                isSubmitted: false
+            }
+        },
+        components: {
+            appFullName: FullName
+        }
     }
 </script>
 
---
+++ form-exercise/src/FullName.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        {{userData.firstName}} {{userData.lastName}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['userData.firstName', 'userData.lastName']
+}
+</script>
