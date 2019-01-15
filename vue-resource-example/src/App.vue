a134254fd4558fbee173fd71bf8193ea92feaa4f
soutoigor <igor_souto@outlook.com>
2019-01-14 22:31:59 -0200

    update vue-resources

--- vue-resource-example/src/App.vue
+++ vue-resource-example/src/App.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Http</h1>
+                <h1>Vue Resource</h1>
 
                 <div class="form-group">
                     <label>Username</label>
@@ -13,7 +13,13 @@
                     <input type="text" class="form-control" v-model="user.email">
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
-
+                <hr>
+                <button class="btn btn-danger" @click="fetchData" >Get Data</button>
+                <br><br>
+                <ul class="list-group" >
+                    <li class="list-group-item" v-for="u in users" :key="u" >
+                        Name: {{u.userName}} - Email: {{u.email}}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -26,16 +32,40 @@
                 user: {
                     userName: '',
                     email: ''
-                }
+                },
+                users: [],
+                resource: {}
             }
         },
         methods: {
             submit(){
-                this.$http.post('https://vuejs-http-e05be.firebaseio.com/data.json', this.user)
+                // this.$http.post('', this.user)
+                // .then( response => {
+                //     console.log(response)
+                // }, error => console.log(error));
+            
+                // this.resource.save({}, this.user);
+                this.resource.saveAlt(this.user);
+            },
+            fetchData(){
+                this.$http.get('')
                 .then( response => {
-                    console.log(response)
-                }, error => console.log(error));
+                   return response.json(); 
+                })
+                .then(data => {
+                    const resultArray = [];
+                    for(let key in data){
+                        resultArray.push(data[key]);
+                    }
+                    this.users = resultArray;
+                })
+            } 
+        },
+        created() {
+            const customActions = {
+                saveAlt: {method: 'POST', url: 'alternative.json'}
             }
+            this.resource = this.$resource('data.json', {}, customActions);
         }
     }
 </script>
--- vue-resource-example/src/main.js
+++ vue-resource-example/src/main.js
@@ -4,7 +4,18 @@ import App from './App.vue'
 
 Vue.use(VueResource);
 
+Vue.http.options.root = 'https://vuejs-http-e05be.firebaseio.com';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if ( request.method == 'POST' ){
+    request.method = 'PUT'
+  }
+  next(response => {
+    response.json = () => {return {messages: response.body}}
+  });
+});
+
 new Vue({
-  el: '#app',
+  el: '#app', 
   render: h => h(App)
 })
